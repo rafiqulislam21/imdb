f72e16cbd86dcf6419a04ccfb8dedda8bc050727
Md Rafiqul Islam <ribijoy19@gmail.com>
2023-09-20 21:44:57 +0200

    webpack

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,21 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
 
     /**
      * @Route("/movies/{name}", name="movieDetails", defaults={"name"=null}, methods={"GET"})
      */
-    public function movieDetails(MovieRepository $movieRepository): Response
+    public function movieDetails(): Response
     {
-      $movies = $movieRepository->findAll();
-      dd($movies);
+        // findAll() SELECT * FROM movies;
+        // find() SELECT * FROM movies where id=5
+        // findBy() SELECT * FROM movies ORDER BY id DESC
+        // findByOne() SELECT * FROM movies WHERE id = 6 
+            // AND title = 'the dark knoght' ORDER By id DESC
+        // count() SELECT COOUNT() from movies WHERE id = 1
+        // $movies = $repository->count(['id'=>5]);
+        // $movies = $repository->findOneBy(['id' => 6], ['id'=> DESC]);
+
+
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->getClassName();
+        dd($movies);
 
         return $this->render('movies/index.html.twig', [
             'controller_name' => 'MoviesController',
